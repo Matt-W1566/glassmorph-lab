9892bb3e54a2611f533910051a48de15831c96c2
Matt-W1566 <matthew.weinstein08@gmail.com>
2025-04-17 12:07:41 -0400

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,19 @@ export default function Home() {
           backgroundImage: `url(${backgroundPicture.src})`,
         }}
       >
+        <div>
+          {/* main div */}
+          <div className="flex items-center justify-center">
+            <h1 className="text-5xl text-white">
+              GLASS
+              <br />
+              MORHPISM
+              <br />
+              LAB
+            </h1>
+          </div>
+          <div></div>
+        </div>
         <div className="glass-container flex items-center justify-center">
           <h1 className="text-3xl text-white">Test</h1>
         </div>
